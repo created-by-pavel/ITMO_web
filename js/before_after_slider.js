7133d31646676d74abfa69776a4481fbf92f33b9
pavel <pavelzavalnyuk11@gmail.com>
2022-12-11 18:18:02 +0300

    feat: add currency conversion

--- js/before_after_slider.js
+++ js/before_after_slider.js
@@ -6,19 +6,13 @@ if (window.innerWidth > 776) {
             skewHack = 0,
             delta = 0;
 
-        // If the parent has .skewed class, set the skewHack var.
         if (parent.className.indexOf('skewed') !== -1) {
             skewHack = 1000;
         }
 
         parent.addEventListener('mousemove', function (event) {
-            // Get the delta between the mouse position and center point.
             delta = (event.clientX - window.innerWidth / 2) * 0.01;
-
-            // Move the handle.
             handle.style.left = event.clientX + delta + 'px';
-
-            // Adjust the top panel width.
             topPanel.style.width = event.clientX + skewHack + delta + 'px';
         });
     });
